5dfc8981c8d6d480b1621958f632cdc79eddd86e
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-28 15:49:51 +0300

    Added day-4 homework

---
+++ MyDictionaryTask/MyDictionaryTask/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryTask
+{
+    class MyDictionary<TKey, TValue> : Exception
+    {
+        TKey[] keysArr;
+        TValue[] valuesArr;
+        private int lenght = 0;
+        public MyDictionary()
+        {
+            keysArr = new TKey[lenght];
+            valuesArr = new TValue[lenght];
+        }
+        public MyDictionary(TKey key, TValue value)
+        {
+            keysArr = new TKey[lenght];
+            valuesArr = new TValue[lenght];
+            Add(key, value);
+        }
+        public int Lenght
+        {
+            get { return lenght; }
+
+        }
+        public void Add(TKey key, TValue value)
+        {
+            if (keysArr.Contains(key))
+            {
+                throw new ArgumentException($"An item with the same key has already been added. Key: {key}'");
+            }
+            TKey[] tempKeysArr = keysArr;
+            TValue[] tempValueArr = valuesArr;
+            keysArr = new TKey[lenght + 1];
+            valuesArr = new TValue[lenght + 1];
+            for (int i = 0; i < lenght; i++)
+            {
+                keysArr[i] = tempKeysArr[i];
+                valuesArr[i] = tempValueArr[i];
+            }
+
+            keysArr[lenght] = key;
+            valuesArr[lenght] = value;
+            lenght++;
+        }
+
+    }
+}
---
+++ MyDictionaryTask/MyDictionaryTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+           
+            myDictionary.Add(1, "Ümit");
+            myDictionary.Add(2, "Halit");
+            myDictionary.Add(3, "Onur");
+            myDictionary.Add(3, "Devrim");
+
+            Console.WriteLine(myDictionary.Lenght);
+            Console.ReadLine();
+        }
+    }
+}
